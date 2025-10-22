69412362743474e354e575725733110eef321e4b
Eric J Crow <EricCrow@pm.me>
2025-10-21 22:58:11 -0700

    style: improved btn

--- components/product/product-detail.tsx
+++ components/product/product-detail.tsx
@@ -309,7 +309,7 @@ export default function ProductDetail({ product, shopifyProduct, options, varian
             <div className="mt-10">
               <AddToCart 
                 product={shopifyProduct}
-                className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+                className="cursor-pointer mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
               />
             </div>
 
